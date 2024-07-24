7a9192c1965d12f444b5c985e027cbcedf8dcb49
hideyoshi515 <hideyoshi515@gmail.com>
2024-07-24 18:13:44 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "com.nechost"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.mina:mina-core:2.2.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
